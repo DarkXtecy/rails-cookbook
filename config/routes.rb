a78d35d2440bb3904acd97b08bf30585c7127847
Mohammed-ZakiUllah Bhingraj <zakibhingraj@yahoo.com>
2025-01-13 14:59:40 +0000

    Update routes to exclude edit, update, and delete actions for categories

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,5 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
-  resources :categories, except: [:edit, :update]
+  resources :categories, except: [:edit, :update, :destroy]
 end
